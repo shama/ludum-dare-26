b9774893a4cd97aabb6b1ef5e62e9c1caec312d1
Kyle Robinson Young <kyle@dontkry.com>
2013-04-27 14:02:57 -0700

    sneaking off to work on game

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var tic = require('tic')();
 var _ = require('lodash');
 var maze = require('./lib/maze');
 var map = require('./maps/level1');
+var createRed = require('./lib/red');
 
 var game = createGame({
   chunkDistance: 2,
+  //generate: function(x, y, z) { return 0; },
   generate: function(x, y, z) {
-    return maze(x, y, z, map);
-  },
-  materials: ['white', 'black', 'red', 'gray'],
+    return maze.generate(x, y, z, map);
+  }.bind(game),
+  materials: ['white', 'black', 'red', 'gray', 'redface', 'redfacemouth', 'marker'],
   texturePath: 'textures/',
   lightsDisabled: true,
   fogDisabled: true,
@@ -18,11 +20,20 @@ var game = createGame({
 var container = document.getElementById('container');
 game.appendTo(container);
 
+// Create a grid for npc to navigate
+var grid = maze.createGrid(map);
+
 // needs more gravity
-game.gravity = [0, -0.0000036 * 10, 0];
-game.friction = 0.1;
+//game.gravity = [0, -0.0000036 * 10, 0];
+//game.friction = 0.1;
+
+// give a copy for easier access
+game.tic = tic;
 
 game.addStats();
+
+// keep track of reds
+var reds = [];
 
 // hide title screen on click
 var title = document.getElementById('title');
@@ -31,7 +42,13 @@ title.addEventListener('click', function() {
 });
 
 // highlight voxels
-require('voxel-highlight')(game, {color: 0xdddddd, animate: true, wireframeOpacity: 0.2});
+var highlighter = require('voxel-highlight')(game, {color: 0xdddddd, animate: true, wireframeOpacity: 0.2});
+highlighter.on('highlight', function(pos) {
+  // antagonize reds
+  reds.forEach(function(r) {
+    r.antagonize(pos);
+  });
+});
 
 // add materials
 //require('./lib/materials')(game);
@@ -51,7 +68,7 @@ mal.yaw.rotation.set(0, Math.PI / 180 * 210, 0);
 mal.possess();
 
 // fly!
-//require('voxel-fly')(game)(game.controls.target());
+require('voxel-fly')(game)(game.controls.target());
 
 // markers
 var markers = require('./lib/markers')(game);
@@ -60,9 +77,15 @@ markerCount.innerHTML = 'x' + markers.max;
 
 // explode voxel on click
 var explode = require('voxel-debris')(game, { power : 1.5 });
-game.on('fire', _.debounce(function(pos) {
+game.on('fire', _.debounce(function() {
   var pos = game.raycast(game.cameraPosition(), game.cameraVector(), 10).voxel;
+  if (!pos) return;
   markerCount.innerHTML = 'x' + markers.place(pos);
+
+  reds.forEach(function(r) {
+    r.navigate(pos);
+  });
+
   //if (erase) explode(pos);
   //else game.createBlock(pos, 1);
 }, 200));
@@ -76,5 +99,15 @@ function ctrlToggle (ev) { erase = !ev.ctrlKey }
 game.on('tick', function(dt) {
   tic.tick(dt);
   //follow.tick(dt);
+
+  if (Object.keys(maze.spawn).length > 0) {
+    Object.keys(maze.spawn).forEach(function(pos) {
+      var type = maze.spawn[pos];
+      if (type === 'red') {
+        reds.push(createRed(game, pos.split('|'), grid));
+      }
+      delete maze.spawn[pos];
+    });
+  }
 });
 
